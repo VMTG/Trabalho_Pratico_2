57d6716da385102ced5bd38b0e1654bbb9fa6d59
VMTG <vitormtg02@ua.pt>
2023-11-01 22:23:31 +0000

    update

--- ar_paint.py
+++ ar_paint.py
@@ -307,7 +307,7 @@ def main():
     started_draw = False
 
     if use_grid:
-        print('Using a grid as a canvas') 
+        print('Using a numbered grid as a canvas') 
         zones, numbers_to_colors = form_Grid(paint_window)
         num_zones = len(zones)
         color_numbers = []
